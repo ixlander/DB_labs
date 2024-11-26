be2b5e4223b68e8b2de60f82a1fd2bded403ba9b
Abumuslim Abakarov <abakarovmuslim26@gmail.com>
2024-11-26 15:47:30 +0500

    upd

--- lab9/queries.sql
+++ lab9/queries.sql
@@ -5,6 +5,7 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+select increase_value(10);
 select increase_value((select cast(max(employees.emp_salary) as integer) from employees));
 
 CREATE OR REPLACE FUNCTION compare_numbers(num1 INTEGER, num2 INTEGER)
@@ -38,24 +39,32 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-SELECT number_series(5);  -- Возвращает '1, 2, 3, 4, 5'
+SELECT number_series(5);
 
 
 CREATE OR REPLACE FUNCTION find_employee(emp_name VARCHAR)
 RETURNS TABLE(emp_id INT, name VARCHAR, department VARCHAR, salary DECIMAL) AS $$
 BEGIN
-    RETURN QUERY SELECT * FROM employees WHERE name = emp_name;
+    RETURN QUERY
+    SELECT e.id, e.name, e.department, e.emp_salary
+    FROM employees e
+    WHERE e.name = emp_name;
 END;
 $$ LANGUAGE plpgsql;
 
-SELECT * FROM find_employee('John Doe');
+SELECT * FROM find_employee('Bob Smith');
 
 CREATE OR REPLACE FUNCTION list_products(category_name VARCHAR)
 RETURNS TABLE(product_id INT, product_name VARCHAR, category VARCHAR, price DECIMAL) AS $$
 BEGIN
-    RETURN QUERY SELECT * FROM products WHERE category = category_name;
+    RETURN QUERY
+    SELECT p.product_id, p.product_name, p.category, p.price
+    FROM products p
+    WHERE p.category = category_name;
 END;
 $$ LANGUAGE plpgsql;
+
+SELECT * FROM list_products('Electronics');
 
 
 CREATE OR REPLACE FUNCTION calculate_bonus(emp_id INTEGER)
@@ -91,13 +100,9 @@ DECLARE
     numeric_result INTEGER;
     string_result TEXT;
 BEGIN
-    -- Numeric calculation
     numeric_result := num1 * 2;
-
-    -- String manipulation
     string_result := 'Hello, ' || str1;
 
-    -- Combine results
     RETURN string_result || ' - Calculation Result: ' || numeric_result;
 END;
 $$ LANGUAGE plpgsql;
